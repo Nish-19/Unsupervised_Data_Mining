0e41322db9dedb659ade9f8e8ad0c7ad853f4c56
Nischal Ashok <41947720+Nish-19@users.noreply.github.com>
2019-12-21 18:41:08 +0530

    Updated all_files_generator.py

--- all_files_generator.py
+++ all_files_generator.py
@@ -1,139 +1,121 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sun Dec 15 10:48:46 2019
+'''
+Used for analysing a directory of html files, it:
+	Extracts data and convertes them to csv
+	Generates heading for each file
+	Finds the heading distribution 
 
-@author: NISCHAL
-"""
-
+'''
 import os
 
-path = 'D:/Text_mining/Htmls/samiran/samiran_resumes'
+# Importing BeautifulSoup
+import bs4
+from bs4 import BeautifulSoup
 
-files = []
-root = ''
-for r, d, f in os.walk(path):
-    root = r
-    for file in f:
-        if '.html' in file:
-            files.append(file)
-           # files.append(os.path.join(r, file))
+# Importing csv
+import csv
 
-for f in files:
-    print(f)
-    from bs4 import BeautifulSoup
-    import bs4
-    file_name = os.path.join(root, f)
-    
-    with open(file_name, 'rb') as fp:
-        soup = BeautifulSoup(fp.read(), features = "lxml")
-    
-    #print(soup.body.contents)
-    body_contentes = soup.body.contents
-    '''for string in soup.body.stripped_strings:
-        print(repr(string))'''
-        
-        
-    '''for content in body_contentes:
-        if content != str:
-            print(content)'''
-            #if content.strings != None:
-             #   print(content.strings)
-    
-    #print(body_contentes[3].string)
-    #print(body_contentes[5].children)
-    
-    
-    #for child in body_contentes[5].children:
-    #    print(child)
-    dict_data = []
-    print(type(body_contentes[0]))
-    for j in range(0, len(body_contentes)):
-        if type(body_contentes[j]) != bs4.element.NavigableString:
-            print(j, '\n', body_contentes[j])
-            res = list(body_contentes[j].children)
-            print(body_contentes[j]['style'])
-            #print(res)
-            inner = []
-            styles = []
-            for i in range(0, len(res)):
-                if type(res[i]) != bs4.element.NavigableString:
-                    #print(res[i].children)
-                    try:
-                        styles.append(res[i]['style'])
-                    except KeyError:
-                        pass
-                    inner.append(list(res[i].children))
-                else:
-                    print(res[i])
-            
-            #print(inner)
-            
-            for k, object in enumerate(inner):
-                all_strings = ''
-                all_strings = all_strings.encode('utf-8')
-                for string in object:
-                    if type(string) == bs4.element.NavigableString:
-                        string = string.encode('utf-8')
-                        all_strings = all_strings + string
-                info = {}
-                #string = string.strip('\n')
-                #string = string.strip('\r')
-                try:
-                    info['String'] = all_strings
-                    info['Style'] = styles[k]
-                    info['DIV-STYLE'] = body_contentes[j]['style']
-                    #info[string] = styles[k]
-                    print(info)
-                    dict_data.append(info)
-                    #print(dict_data)
-                except IndexError:
-                    pass
-                        #print(string)
-    #print(all_strings)
-    #print(info)
-                            
-    #print(dict_data)
-    
-    import csv
-    csv_cloumns = ['String', 'Font Type','Font Style', 'Font Description','Font Size','position', ' border', ' writing-mode', ' left', ' top', ' width', ' height']
-    name = str(f[:f.find('.html')]) + '.csv'
-    csv_file = os.path.join(root, name)
-    try:
-        with open(csv_file, 'w') as csvfile:
-            writer = csv.DictWriter(csvfile, fieldnames=csv_cloumns)
-            writer.writeheader()
-            for data in dict_data:
-                src = data['Style']
-                x = src.find(':')
-                y = src.find('+')
-                z = src.find(';')
-                font_style = src[y+1:z]
-                if font_style.find('font-family') != -1:
-                    font_style = font_style[font_style.find(':')+1:]
-                m=font_style.find('-')
-                font_description = ''
-                if m == -1:
-                    font_description = 'default'
-                else:
-                    font_description = font_style[m+1:]
-                    font_style = font_style[:m]
-                del data['Style']
-                data['Font Type']=(src[x+2:y])
-                data['Font Style']=font_style
-                data['Font Description'] = font_description
-                data['Font Size']=(src[len(src)-4:])
-                lst=(data['DIV-STYLE'].split(';'))
-                del data['DIV-STYLE']
-                for i in range(0,len(lst)-1):
-                    a = lst[i].split(':')
-                    print(a)
-                    data[str(a[0])]=str(a[1])
-                try:
-                    writer.writerow(data)
-                except ValueError:
-                    print('Value Error')
-                    
-    except IOError:
-        print("I/O error")
-    except UnicodeEncodeError:
-        print('unicode error')
+# Importing defaultdict
+from collections import defaultdict
+
+# Importing the html_parser python file
+from html_parser import *
+
+def all_files(output_directory, path):
+	'''
+	Iterates through each html file in the directory, and updates the parsed information in csv
+	Collects all headings of each files and returns them
+
+	Parameters : output_directory - Directory in which all output files will be saved
+				 path 			  - Path of the current location of .html file
+
+	return 	   : all_headings - Dictionary with file name as key and list of headings as value
+	'''
+	files = list()
+	root = ''
+
+	for r, d, f in os.walk(path):
+		root = r
+		for file in f:
+			if '.html' in file:
+				files.append(file)
+
+	#output_directory = os.path.join(path, 'output')
+
+	try:
+		os.mkdir(output_directory)
+	except FileExistsError:
+		print("Folder already exists, for running again delete the folder")
+
+	all_headings = dict()
+
+	for f in files:
+		file_name = os.path.join(path, f)
+		body_contents = file_open(file_name)
+		dict_data = preprocess_data(body_contents)
+		output_path = os.path.join(output_directory, f)
+		write_csv(dict_data, output_path)
+		heading_scores = generate_headings(dict_data)
+		headings = find_headings(heading_scores)
+		all_headings[f] = headings
+
+	#print_all_headings(output_directory, all_headings)
+	return all_headings
+
+def print_all_headings(output_directory, all_headings):
+	'''
+	Prints all the headings of different files into an output text file
+
+	Parameters : output_directory - Directory of the output
+				 all_headings - Dictionary with file name as key and list of headings as value
+	'''
+	text_file = os.path.join(output_directory, 'all_headings.txt')
+	with open(text_file, 'w') as f:
+		print(all_headings, file = f)
+
+def generate_heading_frequency(output_directory, all_headings):
+	'''
+	Generates and prints the heading frequency distribution
+
+	Parameters : output_directory - Directory of the output
+				 all_headings - Dictionary with file name as key and list of headings as value
+	Return : Void
+	'''
+	heading_data = defaultdict(int)
+
+	for file, names in all_headings.items():
+		for string in names:
+			string = string.decode()
+			string = string.strip('\n')
+			string = string.strip('\r')
+			lst = string.split(' ')
+			lst = list(filter(''.__ne__, lst))
+
+			string = ''
+
+			for element in lst:
+				element = element.strip('-')
+				element = element.strip(':')
+
+				if element.isalpha():
+					string = string + element + ' '
+
+			string = string.strip(' ')
+			string = string.upper()
+			heading_data[string] = heading_data[string] + 1
+
+
+	dictionary_file = os.path.join(output_directory, 'headings_data.txt')
+	with open(dictionary_file, 'w') as f:
+		for w in sorted(heading_data, key=heading_data.get, reverse = True):
+			print(w, heading_data[w])
+			print(w, heading_data[w], file = f)
+
+def main():
+	path = 'D:/Text_mining/Htmls/samiran/samiran_resumes'
+	output_directory = os.path.join(path, 'output')
+	all_headings = all_files(output_directory, path)
+	generate_heading_frequency(output_directory, all_headings)
+	print_all_headings(output_directory, all_headings)
+
+if __name__ == '__main__':
+	main()
